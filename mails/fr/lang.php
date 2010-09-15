64ff6a0e3ad7fbe2f296a91740776cb2aac62a36
tLeclerc <tLeclerc@b9a71923-0436-4b27-9f14-aed3839534dd>
2010-09-15 13:59:37 +0000

    git-svn-id: http://svn.prestashop.com/tags/1.3.2.1@2749 b9a71923-0436-4b27-9f14-aed3839534dd

--- mails/fr/lang.php
+++ mails/fr/lang.php
@@ -2,15 +2,31 @@
 
 global $_LANGMAIL;
 $_LANGMAIL = array();
-$_LANGMAIL['Your new admin password'] = 'Votre nouveau mot de passe administrateur';
-$_LANGMAIL['Your password'] = 'Votre nouveau mot de passe';
 $_LANGMAIL['Welcome!'] = 'Bienvenue !';
-$_LANGMAIL['Order confirmation'] = 'Confirmation de commande';
 $_LANGMAIL['Message from contact form'] = 'Message depuis le formulaire de contact';
-$_LANGMAIL['My personal informations'] = 'Mes informations personnelles';
-$_LANGMAIL['Message from a customer'] = 'Message d\'un client';
+$_LANGMAIL['New credit slip regarding your order #'] = '';
+$_LANGMAIL['Product available'] = 'Produit disponible.';
 $_LANGMAIL['Virtual product to download'] = 'Produit virtuel à télécharger';
-$_LANGMAIL['Referral Program'] = 'Programme de parrainage';
 $_LANGMAIL['Package in transit'] = 'Colis en cours d\'envoi';
+$_LANGMAIL['New order'] = '';
+$_LANGMAIL['Order confirmation'] = 'Confirmation de commande';
+$_LANGMAIL['Message from a customer'] = 'Message d\'un client';
+$_LANGMAIL['New message regarding your order'] = '';
+$_LANGMAIL['Your order return state has changed'] = '';
+$_LANGMAIL['Your password'] = 'Votre nouveau mot de passe';
+$_LANGMAIL['Product out of stock'] = '';
+$_LANGMAIL['New voucher regarding your order #'] = '';
+$_LANGMAIL['Happy birthday!'] = 'Joyeuses fêtes';
+$_LANGMAIL['Newsletter confirmation'] = '';
+$_LANGMAIL['Newsletter voucher'] = '';
+$_LANGMAIL['Message from '] = '';
+$_LANGMAIL['Your cart and your discount'] = '';
+$_LANGMAIL['Thanks for your order'] = '';
+$_LANGMAIL['You are one of our best customers'] = '';
+$_LANGMAIL['We miss you'] = '';
+$_LANGMAIL['Error reporting from your PayPalAPI module'] = 'Erreur du module PayPalApi';
+$_LANGMAIL['Congratulations!'] = '';
+$_LANGMAIL['Referral Program'] = '';
+$_LANGMAIL['A friend sent you a link to'] = 'ghjhgjgh';
 
 ?>
